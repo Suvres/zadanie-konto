430f50c0662dc0e2ae7b78741f9843c70703694e
Suvres <bartosz.blyszcz@gmail.com>
2022-09-16 17:10:09 +0200

    Spring init

---
+++ src/main/java/account/api/ZadanieKontoApplication.java
@@ -0,0 +1,13 @@
+package account.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieKontoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanieKontoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/account/api/ZadanieKontoApplicationTests.java
@@ -0,0 +1,13 @@
+package account.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieKontoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
